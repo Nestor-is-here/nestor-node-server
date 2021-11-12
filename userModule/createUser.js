acbba8dfb3c0c0586d28baf58fd61b3763ce9fdd
Daniel Savio <danysavio16@gmail.com>
2021-11-13 03:34:03 +0530

    NES27 post

--- userModule/createUser.js
+++ userModule/createUser.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import userModel from '../models/user_model'
+
 const createUser = express.Router()
 
+createUser.use(express.urlencoded({
+    extended = true
+}))
 
-// createUser.post(/)
+createUser.use(express.json())
+
+createUser.route('/createUser').post((req, res, next) => {
+    res.send(req.body)
+})
